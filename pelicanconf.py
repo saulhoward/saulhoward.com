740e6f3518e7beb00314855225782ec998288a6a
Saul Howard <saul@saulhoward.com>
2023-08-26 14:58:49 +0100

    updates

--- pelicanconf.py
+++ pelicanconf.py
@@ -38,5 +38,10 @@ SOCIAL = (
 
 DEFAULT_PAGINATION = 10
 
+STATIC_PATHS = ["images", "extra/CNAME"]
+EXTRA_PATH_METADATA = {
+    "extra/CNAME": {"path": "CNAME"},
+}
+
 # Uncomment following line if you want document-relative URLs when developing
 # RELATIVE_URLS = True
