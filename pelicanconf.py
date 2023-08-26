2a918f31485c0a0843c54883687489bd0f7b7b76
Saul Howard <saul@saulhoward.com>
2023-08-26 14:45:36 +0100

    use pelican

---
+++ pelicanconf.py
@@ -0,0 +1,42 @@
+AUTHOR = "Saul Howard"
+SITENAME = "Saul Howard"
+
+THEME = "theme"
+
+PATH = "content"
+
+TIMEZONE = "Europe/London"
+
+DEFAULT_LANG = "en"
+
+ARTICLE_URL = "{slug}"
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+DISPLAY_CATEGORIES_ON_MENU = False
+DISPLAY_PAGES_ON_MENU = False
+
+# Blogroll
+LINKS = (
+    ("Clear Line Tech", "https://clearlinetech.com/"),
+    ("X/Twitter", "https://twitter.com/saulhoward"),
+    ("Deep Drama", "https://deepdrama.com/p/jinja/"),
+    ("LinkedIn", "https://www.linkedin.com/in/saulhoward/"),
+    ("Substack", "https://saulhoward.substack.com/"),
+)
+
+# Social widget
+SOCIAL = (
+    ("You can add links in your config file", "#"),
+    ("Another social link", "#"),
+)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
